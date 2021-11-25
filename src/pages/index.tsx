7067b8bc24c016be77a157de7cc183c362210454
Igor Mircic <igor.mircic@live.com>
2021-11-25 23:54:56 +0100

    next.js init with typescript, mui and emotion cache

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+export default function Index() {
+  return (
+    <div>
+      <Typography variant='h1'>Hello World!!</Typography>
+    </div>
+  );
+}
